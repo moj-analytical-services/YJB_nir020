d8ccc4bab0fc3d920eb0a526913da50b4a312bed
nir020 <nick.read@yjb.gov.uk>
2021-04-28 08:55:23 +0000

    Next test

--- ui.R
+++ ui.R
@@ -4,10 +4,25 @@ library(ggplot2)
 library(shiny)
 library(s3tools)
 
-
 Children <-s3tools::s3_path_to_full_df("alpha-yjb-stats/Children.csv")
 
-ui <- fluidPage(
-  max(Children$YOT)
+YOT<-unique(Children$YOT)
+Eth<-unique(Children$Ethnicity_Group)
+Year<-unique(Children$Financial_Year)
 
+ui <- dashboardPage(
+  dashboardHeader(title = "Asset Plus Tool"),
+  dashboardSidebar(selectInput("YOTs","Select YOT", choices = YOT),selectInput("Year","Select financial year", choices = Year) ),
+  
+  dashboardBody(
+    
+    fluidRow(valueBoxOutput(width=5,"value1")),
+    fluidRow(valueBoxOutput(width=3,"value2"),valueBoxOutput(width=3,"value3"),valueBoxOutput(width=3,"value4")),
+    fluidRow(box(width=8,title="Trends breakdown",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot4",height ="200px"))),
+    fluidRow(box(width=2,title="Gender breakdown",status = "primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height ="200px")),
+             box(width=5,title="Ethnicity breakdown",status = "primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot2",height ="200px")),
+             box(width=2,title="Age breakdown",status = "primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot3",height ="200px"))
+    )
+    
+  )
 )
